edbdae3ab40eb35fda1fe6bc074c4543196c6108
SmartFoxServer <30838007+SmartFoxServer@users.noreply.github.com>
2024-01-09 16:31:22 +0100

    Add files via upload

---
+++ scripts/Characters/MuzzleFlash.cs
@@ -0,0 +1,17 @@
+using Godot;
+/**
+ * ------------------------------------------------------
+ * This method displays the muzzle flash
+ * ------------------------------------------------------
+ */
+public partial class MuzzleFlash : Node3D {
+    [Export] public NodePath ParticlesPath { get;set; }
+    [Export] public float LifeTime { get;set; } = 0.3f;
+
+    public async override void _Ready() {
+        this.GetNode<GpuParticles3D>(this.ParticlesPath).Emitting = true;
+
+        await ToSignal(this.GetTree().CreateTimer(this.LifeTime), "timeout");
+        this.QueueFree();
+    }
+}
